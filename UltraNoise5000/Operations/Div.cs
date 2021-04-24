fc33a879ef3a82f734a77cf16adc80de5c76e034
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2021-04-24 19:12:20 +0300

    Fix Div operation NaN

--- UltraNoise5000/Operations/Div.cs
+++ UltraNoise5000/Operations/Div.cs
@@ -1,12 +1,15 @@
 using NoiseUltra.Nodes;
+using UnityEngine;
 
 namespace NoiseUltra.Operations
 {
     public class Div : NodeDoubleInputOutput
     {
+        public static float NaNCheck(float a) => Mathf.Approximately(0f, a) ? Mathf.Epsilon : a;
+        
         protected override float ExecuteOperation(float strengthenedA, float strengthenedB)
         {
-            return strengthenedA / strengthenedB;
+            return strengthenedA / NaNCheck(strengthenedB);
         }
     }
 }
