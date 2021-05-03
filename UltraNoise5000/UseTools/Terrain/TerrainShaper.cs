bc9a6a2b590ce861ac420aa81454ed67c4fa6d85
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2021-05-04 00:33:24 +0300

    small optimizations terrainshaper

--- UltraNoise5000/UseTools/Terrain/TerrainShaper.cs
+++ UltraNoise5000/UseTools/Terrain/TerrainShaper.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace NoiseUltra.Tools.Terrains
 {
@@ -12,30 +14,35 @@ namespace NoiseUltra.Tools.Terrains
             _heightMap = new float[resolution, resolution];
             progress.SetSize(resolution * resolution);
             var position = transform.position;
-            
+            var relativeSize = GetRelativeSize();
+            yield return Operate(resolution, position, relativeSize);
+            GetTerrainData().SetHeights(0, 0, _heightMap);
+            progress.Reset();
+        }
+
+        private IEnumerator Operate(int resolution, Vector3 position, float relativeSize)
+        {
             for (var x = 0; x < resolution; x++)
             {
+                var xPos = x * relativeSize;
                 for (var y = 0; y < resolution; y++)
                 {
-                    var xPos = x * GetRelativeSize();
-                    var yPos = y * GetRelativeSize();
+                    var yPos = y * relativeSize;
             
                     if (useWorldPos)
                     {
                         xPos += position.x;
                         yPos += position.z;
                     }
-            
+                    
                     _heightMap[y, x] = GetSample(xPos, yPos);
+                    
                     if (!progress.TryProcess())
                     {
                         yield return progress.ResetIteraction();
                     }
                 }
             }
-            
-            GetTerrainData().SetHeights(0, 0, _heightMap);
-            progress.Reset();
         }
     }
 }
