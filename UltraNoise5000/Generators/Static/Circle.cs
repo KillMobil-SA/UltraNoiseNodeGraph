d6eab4137acd6edab6976a6608904b9a34861c16
Ycaro Weschenfelder Rodrigues <ywrycaro@gmail.com>
2021-05-03 23:31:09 +0300

    Made Circle work with curve

--- UltraNoise5000/Generators/Static/Circle.cs
+++ UltraNoise5000/Generators/Static/Circle.cs
@@ -1,4 +1,5 @@
 ﻿using NoiseUltra.Nodes;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace NoiseUltra.Generators.Static
@@ -8,21 +9,42 @@ namespace NoiseUltra.Generators.Static
     {
         private const float Default = 100;
         [SerializeField] private Vector2 center = new Vector2(Default, Default);
-        [SerializeField] private float radius = Default;
+        [SerializeField, OnValueChanged(nameof(RebuildCurve))] private float radius = Default;
         [SerializeField] private bool hardEdges;
+        [SerializeField] private AnimationCurve heightCurve = AnimationCurve.Linear(0, 0, Default, 1);
+
 
         public override float GetSample(float x)
         {
             return 1;
         }
 
+        protected override void Init()
+        {
+            base.Init();
+            RebuildCurve();
+        }
+
+        [Button]
+        private void RebuildCurve()
+        {
+            heightCurve = AnimationCurve.Linear(0, 0, radius, 1);
+        }
+
         public override float GetSample(float x, float y)
         {
             var distance = Vector2.Distance(center, new Vector2(x, y));
+            
             var isInRange = distance < radius;
-            if (hardEdges) return isInRange ? 1 : 0;
+            if (hardEdges)
+            {
+                return isInRange ? 1 : 0;
+            }
 
-            if (isInRange) return 1 - distance / radius;
+            if (isInRange)
+            {
+                return 1 - heightCurve.Evaluate(distance);
+            }
 
             return 0;
         }
@@ -32,9 +54,15 @@ namespace NoiseUltra.Generators.Static
             var distance = Vector3.Distance(center, new Vector3(x, y, z));
             var isInRange = distance < radius;
 
-            if (hardEdges) return isInRange ? 1 : 0;
+            if (hardEdges)
+            {
+                return isInRange ? 1 : 0;
+            }
 
-            if (distance < radius) return 1 - distance / radius;
+            if (distance < radius)
+            {
+                return 1 - heightCurve.Evaluate(distance);
+            }
 
             return 0;
         }
