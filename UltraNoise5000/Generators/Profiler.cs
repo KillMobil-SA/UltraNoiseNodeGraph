baf058323cdc23016b0b2299bd64ebbf0152a129
ycarowr <ywrycaro@gmail.com>
2021-10-30 21:53:18 +0200

    add colorful profiler

--- UltraNoise5000/Generators/Profiler.cs
+++ UltraNoise5000/Generators/Profiler.cs
@@ -5,15 +5,20 @@ namespace NoiseUltra.Generators
 {
     public static class Profiler
     {
+        private const string CLOSE_LOG_COLOR = "</color>";
+        private const string OPEN_LOG_COLOR = "<color=#{0}>";
+        private static readonly string COLOR = ColorUtility.ToHtmlStringRGB(new Color(0, 0.8f, 0));
         public static DateTime begin;
         public static void Start()
         {
             begin = DateTime.Now;
         }
 
-        public static void End(string context)
+        public static void End(string context = "")
         {
-            Debug.Log($"Elapsed Time: {(DateTime.Now.Subtract(begin).TotalSeconds)} Seconds {context}");
+            Debug.Log(string.Format(OPEN_LOG_COLOR, COLOR)
+                      + $"Elapsed Time: {(DateTime.Now.Subtract(begin).TotalSeconds)} Seconds {context}"
+                      + CLOSE_LOG_COLOR);
         }
     }
 }
