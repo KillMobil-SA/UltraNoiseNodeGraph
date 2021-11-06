43063942af84efbcc104f98c1d446f40e78fd23c
ycarowr <ywrycaro@gmail.com>
2021-11-06 12:59:27 +0100

    Add wait class wrapper

---
+++ UltraNoise5000/UseTools/Utils/Wait.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NoiseUltra.Tools
+{
+    public static class Wait
+    {
+        public static readonly WaitForSeconds WAIT_ONE_SECOND = new WaitForSeconds(1);
+    }
+}
