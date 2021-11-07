c90433cf02a6a2402e86f724541d884d44e60d7c
ycarowr <ywrycaro@gmail.com>
2021-11-07 19:07:11 +0100

    Disable Profiler

--- UltraNoise5000/UseTools/Utils/Profiler.cs
+++ UltraNoise5000/UseTools/Utils/Profiler.cs
@@ -5,20 +5,27 @@ namespace NoiseUltra.Generators
 {
     public static class Profiler
     {
+        private static bool IsEnabled { get; } = false;
         private const string CLOSE_LOG_COLOR = "</color>";
         private const string OPEN_LOG_COLOR = "<color=#{0}>";
         private static readonly string COLOR = ColorUtility.ToHtmlStringRGB(new Color(0, 0.8f, 0));
         public static DateTime begin;
         public static void Start()
         {
-            begin = DateTime.Now;
+            if (IsEnabled)
+            {
+                begin = DateTime.Now;
+            }
         }
 
         public static void End(string context = "")
         {
-            Debug.Log(string.Format(OPEN_LOG_COLOR, COLOR)
-                      + $"Elapsed Time: {(DateTime.Now.Subtract(begin).TotalSeconds)} Seconds {context}"
-                      + CLOSE_LOG_COLOR);
+            if(IsEnabled)
+            {
+                Debug.Log(string.Format(OPEN_LOG_COLOR, COLOR)
+                       + $"Elapsed Time: {(DateTime.Now.Subtract(begin).TotalSeconds)} Seconds {context}"
+                       + CLOSE_LOG_COLOR);
+            }
         }
     }
 }
