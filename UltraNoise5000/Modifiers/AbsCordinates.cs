4b234802dfa789b0a13ed61002846a70b43c4c99
Sotiris Gyftopoulos <info@killmobil.com>
2022-01-10 18:22:29 +0200

    Abs Cordinates Node

---
+++ UltraNoise5000/Modifiers/AbsCordinates.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using NoiseUltra.Nodes;
+using Sirenix.OdinInspector;
+
+
+namespace NoiseUltra.Modifiers
+{
+    [NodeTint(NodeColor.MODIFIER)]
+    public class AbsCordinates :NodeInputOutput
+    {
+        
+        public override float GetSample(float x)
+        {
+            
+            return base.GetSample(Mathf.Abs(x));
+        }
+
+        public override float GetSample(float x, float y)
+        {
+            
+            return base.GetSample(Mathf.Abs(x), Mathf.Abs(y));
+        }
+
+        public override float GetSample(float x, float y, float z)
+        {
+            
+            return  base.GetSample(Mathf.Abs(x), Mathf.Abs(y), Mathf.Abs(z) );
+        }
+        
+    }
+}
